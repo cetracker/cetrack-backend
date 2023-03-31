d9baff45a984b76e44e5a8b4fc59a1de90962358
Meinhard Ritscher <unreachable@gmx.net>
2023-03-31 09:41:53 +0200

    Add constraint to bike on part type

--- src/main/resources/db/migration/mysql/V1.1__create_bike_table.sql
+++ src/main/resources/db/migration/mysql/V1.1__create_bike_table.sql
@@ -9,3 +9,10 @@ CREATE TABLE IF NOT EXISTS `bike`
   PRIMARY KEY (`id`)
 )
 DEFAULT CHARACTER SET = 'utf8mb4' COLLATE 'utf8mb4_0900_ai_ci';
+
+ALTER TABLE `part_type` ADD
+    CONSTRAINT `pt_fk_bike_id`
+        FOREIGN KEY (`bike_id`)
+        REFERENCES `bike`(`id`)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION;
