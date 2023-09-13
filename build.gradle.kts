aacea402e58f8e69fd58e7abc5c863f8a59ea395
Meinhard Ritscher <unreachable@gmx.net>
2023-09-13 16:49:18 +0200

    Bump Kotlin dependencies from 1.9.0 to 1.9.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,10 +14,10 @@ plugins {
 
     id("com.bmuschko.docker-remote-api") version "9.3.2"
 
-    kotlin("jvm") version "1.9.0"
-    kotlin("plugin.spring") version "1.9.0"
+    kotlin("jvm") version "1.9.10"
+    kotlin("plugin.spring") version "1.9.10"
     kotlin("plugin.jpa") version "1.9.10"
-    kotlin("plugin.allopen") version "1.9.0" // https://spring.io/guides/tutorials/spring-boot-kotlin/
+    kotlin("plugin.allopen") version "1.9.10" // https://spring.io/guides/tutorials/spring-boot-kotlin/
     /* lombok https://kotlinlang.org/docs/lombok.html
     kotlin("plugin.lombok") version "1.8.0"
     id("io.freefair.lombok") version "5.3.0"
