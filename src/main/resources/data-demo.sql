ab78b66a300cba7829ed5f7fb130341ec4745166
Meinhard Ritscher <unreachable@gmx.net>
2025-02-02 17:02:25 +0100

    Extend example data with second bike and a shared part between bikes

--- src/main/resources/data-demo.sql
+++ src/main/resources/data-demo.sql
@@ -1,6 +1,9 @@
-INSERT INTO BIKE (id, manufacturer, model, bought_at, created_at) VALUES('70556a9d-bf01-42eb-b882-b5938fff7023', 'Vaust', 'Fragola', '2006-06-15 14:00:00', LOCALTIMESTAMP());
+INSERT INTO BIKE (id, manufacturer, model, bought_at, created_at) VALUES
+      ('70556a9d-bf01-42eb-b882-b5938fff7023', 'Vaust', 'Fragola', '2006-06-15 14:00:00', LOCALTIMESTAMP()),
+      ('99a133db-4f9b-4a5b-b6f9-8a5ec96d9db7', 'Cycle', 'TT', '2010-08-15 12:00:00', LOCALTIMESTAMP());
 
-INSERT INTO PART (id, name, created_at, bought_at) VALUES('ca68d5a1-cb85-4e4a-a719-4aa380b76325', 'Rimebreak Campa Veloce front', LOCALTIMESTAMP(), null),
+INSERT INTO PART (id, name, created_at, bought_at) VALUES
+      ('ca68d5a1-cb85-4e4a-a719-4aa380b76325', 'Rimebreak Campa Veloce front', LOCALTIMESTAMP(), null),
       ('f8d98a1d-ab33-4df1-b6c5-eefdf4aa3dca', 'Rimbreak Campa Veloce rear', LOCALTIMESTAMP(), null),
       ('f351b44c-a49b-4871-bd0f-50f7af790b8c', 'Derailleur Campa Chorus front', LOCALTIMESTAMP(), null),
       ('d33a6e96-eb63-4279-8a0c-521b8a4f25d4', 'Derailleur Campa Veloce rear', LOCALTIMESTAMP(), null),
@@ -14,10 +17,13 @@ INSERT INTO PART (id, name, created_at, bought_at) VALUES('ca68d5a1-cb85-4e4a-a7
       ('5cb5afb9-3e1a-4f10-8082-6d72b7b27335', 'Rear Wheel DT-SWiss', LOCALTIMESTAMP(), null),
       ('dcbdcfc6-8c0c-4677-a5e4-61b4dc01f526', 'Front Wheel Campa Zonda', LOCALTIMESTAMP(), null),
       ('80bbc300-5475-4932-8296-c00aaeaa9e5f', 'Rear Wheel Campa Zonda', LOCALTIMESTAMP(), null),
-      ('5fce029f-1e0d-47d0-b220-e604cafc5451', 'Saddle Selle Italia', LOCALTIMESTAMP(), null);
+      ('5fce029f-1e0d-47d0-b220-e604cafc5451', 'Saddle Selle Italia', LOCALTIMESTAMP(), null),
+      ('35300fd6-751c-42eb-8632-30ce2646958c', 'TT Front Wheel', LOCALTIMESTAMP(), null);
 
-INSERT INTO PART_TYPE(id, bike_id, mandatory, name, created_at)
-VALUES('6296981d-92e4-45d3-b26c-331a58284e21', '70556a9d-bf01-42eb-b882-b5938fff7023', '1', 'Chain', LOCALTIMESTAMP()),
+INSERT INTO PART_TYPE(id, bike_id, mandatory, name, created_at) VALUES
+      ('82eabc08-6397-46fd-8581-98d9f18a83ff', '99a133db-4f9b-4a5b-b6f9-8a5ec96d9db7', '1', 'Front Wheel', LOCALTIMESTAMP()),
+      ('c1c2c16b-1890-43ad-8aee-f8e5c9f68928', '99a133db-4f9b-4a5b-b6f9-8a5ec96d9db7', '1', 'Saddle', LOCALTIMESTAMP()),
+      ('6296981d-92e4-45d3-b26c-331a58284e21', '70556a9d-bf01-42eb-b882-b5938fff7023', '1', 'Chain', LOCALTIMESTAMP()),
       ('59143611-6a57-4e92-8fc6-de7e1a434625', '70556a9d-bf01-42eb-b882-b5938fff7023', '1', 'Saddle', LOCALTIMESTAMP()),
       ('821f1d71-2515-47d7-b771-2badb3ffa2f9', '70556a9d-bf01-42eb-b882-b5938fff7023', '1', 'Front Derailleur', LOCALTIMESTAMP()),
       ('33905c08-a045-4e43-aefa-2c2963ffe582', '70556a9d-bf01-42eb-b882-b5938fff7023', '1', 'Rear Derailleur', LOCALTIMESTAMP()),
@@ -29,9 +35,11 @@ VALUES('6296981d-92e4-45d3-b26c-331a58284e21', '70556a9d-bf01-42eb-b882-b5938fff
       ('c47e51e6-0f07-4b4b-8390-9b065e59e197', '70556a9d-bf01-42eb-b882-b5938fff7023', '1', 'Front Rimbrake', LOCALTIMESTAMP()),
       ('24996c8c-c559-45a2-8e4a-116f1a8f33ae', '70556a9d-bf01-42eb-b882-b5938fff7023', '1', 'Rear Rimbrake', LOCALTIMESTAMP());
 
-INSERT INTO PART_PART_TYPES(part_id, part_type_id, valid_from, valid_until)
-VALUES('75efe2d4-bc85-430b-af26-1deb58409b43', '6296981d-92e4-45d3-b26c-331a58284e21', '2006-06-15 14:00:00', null ),
-      ('5fce029f-1e0d-47d0-b220-e604cafc5451', '59143611-6a57-4e92-8fc6-de7e1a434625', '2006-06-15 13:59:59', null ),
+INSERT INTO PART_PART_TYPES(part_id, part_type_id, valid_from, valid_until) VALUES
+      ('5fce029f-1e0d-47d0-b220-e604cafc5451', 'c1c2c16b-1890-43ad-8aee-f8e5c9f68928', '2011-08-10 12:00:00', null ),
+      ('35300fd6-751c-42eb-8632-30ce2646958c', '82eabc08-6397-46fd-8581-98d9f18a83ff', '2010-08-15 12:00:00', null ),
+      ('75efe2d4-bc85-430b-af26-1deb58409b43', '6296981d-92e4-45d3-b26c-331a58284e21', '2006-06-15 14:00:00', null ),
+      ('5fce029f-1e0d-47d0-b220-e604cafc5451', '59143611-6a57-4e92-8fc6-de7e1a434625', '2006-06-15 13:59:59', '2011-08-09 23:59:59' ),
       ('ca68d5a1-cb85-4e4a-a719-4aa380b76325', 'c47e51e6-0f07-4b4b-8390-9b065e59e197', '2006-06-15 13:59:59', null ),
       ('f8d98a1d-ab33-4df1-b6c5-eefdf4aa3dca', '24996c8c-c559-45a2-8e4a-116f1a8f33ae', '2006-06-15 13:59:59', null ),
       ('4bbf00a2-7539-4d5f-944f-aa524aa1323a', '12b6f43b-af8f-4d2d-8ae4-2d5a98c825a6', '2006-06-15 13:59:59', null ),
@@ -48,8 +56,8 @@ VALUES('75efe2d4-bc85-430b-af26-1deb58409b43', '6296981d-92e4-45d3-b26c-331a5828
       ('d33a6e96-eb63-4279-8a0c-521b8a4f25d4', '33905c08-a045-4e43-aefa-2c2963ffe582', '2006-06-15 14:00:00', null );
 
 
-INSERT INTO TOUR (id, title, start_year, start_month, start_day, started_at, distance, duration_moving, alt_up, alt_down, power_total, bike_id, created_at)
-VALUES('8e449d84-5373-4285-9f65-bcd49f86c67e', 'Passo Giau',  '2010', '06', '07','2010-06-07 10:22:46', 53120, 7614, '192', '192', '5043', '70556a9d-bf01-42eb-b882-b5938fff7023', LOCALTIMESTAMP()),
+INSERT INTO TOUR (id, title, start_year, start_month, start_day, started_at, distance, duration_moving, alt_up, alt_down, power_total, bike_id, created_at) VALUES
+      ('8e449d84-5373-4285-9f65-bcd49f86c67e', 'Passo Giau',  '2010', '06', '07','2010-06-07 10:22:46', 53120, 7614, '192', '192', '5043', '70556a9d-bf01-42eb-b882-b5938fff7023', LOCALTIMESTAMP()),
       ('2bc77a21-e0b7-4600-a5fa-385b0a5c5e21', 'Tourmalido',  '2010', '06', '11','2010-06-11 09:50:08', 43160, 5534, '192', '192', '5043', '70556a9d-bf01-42eb-b882-b5938fff7023', LOCALTIMESTAMP()),
       ('f1701850-953e-42e9-868a-dd3ccbf9bd53', 'Demo Tour 01','2010', '06', '26','2010-06-26 07:45:24', 93203, 12520, '192', '192', '5043', '70556a9d-bf01-42eb-b882-b5938fff7023', LOCALTIMESTAMP()),
       ('bd40eae5-9aab-4d7a-a426-5f39e98556b6', 'Demo Tour 02','2010', '07', '01','2010-07-01 09:46:06', 103002, 13237, '192', '192', '5043', '70556a9d-bf01-42eb-b882-b5938fff7023', LOCALTIMESTAMP()),
@@ -81,4 +89,5 @@ VALUES('8e449d84-5373-4285-9f65-bcd49f86c67e', 'Passo Giau',  '2010', '06', '07'
       ('4dd224e8-e168-4124-94da-c12ed949c789', 'Demo Tour 11-02', '2011', '04', '05', '2011-04-05 13:47:07', 54280, 5945, '192', '192', '5043', '70556a9d-bf01-42eb-b882-b5938fff7023', LOCALTIMESTAMP()),
       ('fe1b57da-2132-46dd-b1fc-cb7787be9675', 'Demo Tour 11-03', '2011', '05', '05', '2011-05-05 13:47:07', 65318, 7290, '192', '192', '5043', '70556a9d-bf01-42eb-b882-b5938fff7023', LOCALTIMESTAMP()),
       ('81bfadaa-67bb-485e-8cf6-a420d81885b4', 'Demo Tour 11-04', '2011', '06', '05', '2011-06-05 13:47:07', 76438, 9234, '192', '192', '5043', '70556a9d-bf01-42eb-b882-b5938fff7023', LOCALTIMESTAMP()),
-      ('c7d2d0d8-8af8-44d5-b073-c91ea0b99fc0', 'Demo Tour 11-05', '2011', '07', '05', '2011-07-05 13:47:07', 87835, 9999, '192', '192', '5043', '70556a9d-bf01-42eb-b882-b5938fff7023', LOCALTIMESTAMP());
+      ('c7d2d0d8-8af8-44d5-b073-c91ea0b99fc0', 'Demo Tour 11-05', '2011', '07', '05', '2011-07-05 13:47:07', 87835, 9999, '192', '192', '5043', '70556a9d-bf01-42eb-b882-b5938fff7023', LOCALTIMESTAMP()),
+      ('d10d8724-0d12-431c-813d-5a93328428e3', 'Demo TT-Tour 11-01', '2011', '08', '25', '2011-08-25 11:17:23', 10000, 8999, '150', '150', '444', '99a133db-4f9b-4a5b-b6f9-8a5ec96d9db7', LOCALTIMESTAMP());
